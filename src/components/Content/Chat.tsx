f9d012e5f04b3a8451abddb89d7b1aade1ac9ca5
Krystian G <144816009+vertira@users.noreply.github.com>
2024-10-04 18:03:16 +0200

    Update Chat.tsx

--- src/components/Content/Chat.tsx
+++ src/components/Content/Chat.tsx
@@ -10,19 +10,23 @@ export const Chat = () => {
   return (
     <motion.div
       initial={{ y: 440, x: 400 }}
-      animate={isOpen ? { y: 0, x: 0 } : { y: isMobile ? 440 : 440, x: isMobile ? 160 : 400 }}
+      animate={
+        isOpen
+          ? { y: 0, x: 0 }
+          : { y: isMobile ? 440 : 440, x: isMobile ? 160 : 400 }
+      }
       transition={{ duration: 0.5 }}
       className="fixed bottom-0 right-0"
     >
       <div className="relative">
         <iframe
-          src="http://localhost:3000/"
+          src="https://portfolio-chat-rag.vercel.app/"
           width="600"
           height="400"
           className="h-[500px] w-[350px] rounded-xl md:h-[500px] md:w-[600px]"
         ></iframe>
         <div
-          className={`swallow__icon absolute left-1/2 max-md:-translate-x-7 md:left-1/4 top-0 w-full translate-y-1/4`}
+          className={`swallow__icon absolute left-1/2 top-0 w-full translate-y-1/4 max-md:-translate-x-7 md:left-1/4`}
           onClick={() => setIsOpen((prev) => !prev)}
         >
           <ChevronDown
